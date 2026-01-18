d5467c30e342bee179ffd6f64be1342b424f7e01
Utsav Garg <er.utsavgarg@gmail.com>
2026-01-18 20:02:38 +0530

    Add google analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,6 +73,10 @@ const config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        gtag: {
+          trackingID: 'G-98116Q442C', // Replace with your Google Analytics Tracking ID
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
